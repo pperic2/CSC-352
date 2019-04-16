b862cbb9e4c9747e11a1e4df8b60e1f8c0230405
Daniel K Goodrum <40883254+Danielg686@users.noreply.github.com>
2019-04-16 15:15:48 -0500

    SE352 Group 5
    Clean Build and running spring boot skeleton code.

---
+++ SE352/src/main/java/group5/toolshare/ToolshareApplication.java
@@ -0,0 +1,13 @@
+package group5.toolshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolshareApplication.class, args);
+    }
+
+}
